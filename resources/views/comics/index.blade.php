fe82aa7d78693c70613732cd522a23465f3ae276
michaelb2001 <brunco.michael@gmail.com>
2022-03-09 15:39:46 +0100

    view comics table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,14 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+@extends('layout.base')
 
-    <link rel="stylesheet" href={{ asset('css/app.css') }}>
-</head>
-<body>
-    
-</body>
-</html>
+@section('title','comics-table')
+
+@section('content')
+<table class="table table-bordered">
+    <thead>
+      <tr>
+        <th scope="col"></th>
+        <th scope="col">title</th>
+        <th scope="col">description</th>
+        <th scope="col">thumb</th>
+        <th scope="col">price</th>
+        <th scope="col">series</th>
+        <th scope="col">sale_date</th>
+        <th scope="col">type</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($comics as $comic)
+        <tr>
+            <th scope="row">{{$comic->id}}</th>
+            <td>{{$comic->title}}</th>
+            <td>{{$comic->description}}</th>
+            <td><img src="{{$comic->thumb}}" alt=""></th>
+            <td>{{$comic->price}}</th>
+            <td>{{$comic->series}}</th>
+            <td>{!!$comic->sale_date!!}</th>
+            <td>{!!$comic->type!!}</th>
+        </tr>
+        @endforeach
+      
+    </tbody>
+  </table>
+@endsection
